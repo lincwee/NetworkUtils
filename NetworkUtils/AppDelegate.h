760bca541882444a0c21aa0a7bd0bced1d72a027
linwei.hao <linwei.hao@qunar.com>
2016-07-07 14:53:40 +0800

    Initial Commit

---
+++ NetworkUtils/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworkUtils
+//
+//  Created by 郝 林巍 on 16/7/7.
+//  Copyright © 2016年 lincwee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetworkUtils/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetworkUtils
+//
+//  Created by 郝 林巍 on 16/7/7.
+//  Copyright © 2016年 lincwee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
